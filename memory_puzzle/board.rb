841b89be20689200a6f61deb985b3fc4e7e4530e
ishsgit <aishtahad25@gmail.com>
2022-09-27 14:39:38 -0400

    board populate in progress

--- memory_puzzle/board.rb
+++ memory_puzzle/board.rb
@@ -1,6 +1,65 @@
+require "byebug"
+
 class Board
+    def self.random_cards
+        alpha=("a".."z").to_a
+        shuffle_l=alpha.shuffle
+        eight_letters=[]
+        shuffle_l.each_with_index do |ele,idx|
+            if idx<8   
+                eight_letters<<ele
+            end
+        end
+        eight_letters
+    end
+    
     def initialize
         @grid = Array.new(4) {Array.new(4)}
-        @card = Card.new
+        # @card = Card.new
+        @cards=[]
+        @size=@grid.length*@grid.length
+        @letters=Board::random_cards
     end
+ 
+    # def place_random_ships
+    #     length=@grid.length
+    
+    #     reduced=@size*0.25
+    #     i=0
+    #     while i<reduced
+            
+    #         row=rand(0...length)
+    #         col=rand(0...length)
+    #         if self[[row,col]]!=:S
+    #         self[[row,col]]=:S
+    #         i+=1
+    #         end
+    #     end
+    # end
+
+    def populate
+        idx=0
+        j=0
+        until @cards.length==@size
+            # row=rand(0...@grid.length)
+            # col=rand(0...@grid.length)
+            #  @cards.concat(Board::random_cards)
+             
+            #  @grid[i]=@cards.map do |row|
+            #     sub=[]
+            #     @cards.each_with_index |ele,idx|
+            @cards<<Board::random_cards[idx]
+            if idx!=0 && idx%4==0
+                @grid[idx].concat(@cards)
+                debugger
+            end
+            idx+=1
+
+        end
+    end
+
+
 end
+
+board=Board.new
+board.populate
