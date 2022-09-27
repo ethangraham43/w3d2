aed59bf554daa2174e3fdfff7535f3d0d947c896
ishsgit <aishtahad25@gmail.com>
2022-09-27 15:43:57 -0400

    Board #render #won

--- memory_puzzle/board.rb
+++ memory_puzzle/board.rb
@@ -1,35 +1,16 @@
 require "byebug"
 
-<<<<<<< HEAD
-# class Board
-#     def self.random_cards
-#         alpha=("a".."z").to_a
-#         shuffle_l=alpha.shuffle
-#         eight_letters=[]
-#         shuffle_l.each_with_index do |ele,idx|
-#             if idx<8   
-#                 eight_letters<<ele
-#             end
-#         end
-#         eight_letters
-#     end
-=======
 class Board
->>>>>>> 923ebd627b3c45814e706cd2c86f836ed5b090f7
     
     def initialize
         @grid = Array.new(4) {Array.new(4)}
-        # @card = Card.new
+        @card = Card.new
         @cards=[:A, :B, :C, :D, :E, :F, :G, :H, :A, :B, :C, :D, :E, :F, :G, :H]
         @size=@grid.length*@grid.length
+
     end
-<<<<<<< HEAD
- 
-    
-=======
 
 
->>>>>>> 923ebd627b3c45814e706cd2c86f836ed5b090f7
     def populate
         i = 0
         while i < @size
@@ -43,7 +24,23 @@ class Board
         @grid
     end
 
+    def render
+         @grid.each do |row|
+            print row
+            print "\n"
+         end
+    end
+
+    #Cards should card instances
+    def won?
+        @cards.all? {|card| card.reveal}
+    end
+
+
 end
+
+   
 
 board=Board.new
 board.populate
+board.render
