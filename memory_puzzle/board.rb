fc5ecac5557ab0ca06da6e9176d5843d5cf6878c
Ethan Graham <ethangraham43@gmail.com>
2022-09-27 16:44:55 -0400

    commit

--- memory_puzzle/board.rb
+++ memory_puzzle/board.rb
@@ -1,21 +1,7 @@
 require "byebug"
 
-<<<<<<< HEAD
 # class Board
-#     def self.random_cards
-#         alpha=("a".."z").to_a
-#         shuffle_l=alpha.shuffle
-#         eight_letters=[]
-#         shuffle_l.each_with_index do |ele,idx|
-#             if idx<8   
-#                 eight_letters<<ele
-#             end
-#         end
-#         eight_letters
-#     end
-=======
 class Board
->>>>>>> 923ebd627b3c45814e706cd2c86f836ed5b090f7
     
     def initialize
         @grid = Array.new(4) {Array.new(4)}
@@ -23,13 +9,8 @@ class Board
         @cards=[:A, :B, :C, :D, :E, :F, :G, :H, :A, :B, :C, :D, :E, :F, :G, :H]
         @size=@grid.length*@grid.length
     end
-<<<<<<< HEAD
- 
-    
-=======
 
 
->>>>>>> 923ebd627b3c45814e706cd2c86f836ed5b090f7
     def populate
         i = 0
         while i < @size
@@ -44,6 +25,7 @@ class Board
     end
 
 end
+
 
 board=Board.new
 board.populate
